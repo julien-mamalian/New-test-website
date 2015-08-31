8608700fde51f93ccd489eb40f9b3f6f402b5714
julien mamalian <julien.mamalian@gmail.com>
2015-08-31 21:05:51 +0200

    galerie à faire

--- source/javascripts/menu_4.js
+++ source/javascripts/menu_4.js
@@ -1,47 +1,22 @@
 $(document).ready(function(){
-   var height_all = $('#contain_under_home_3').height();
-  $('#food_11').css({
-    'height': height_all * 0.84
-  })
-  $('.main').hide();
-  $('.dessert').hide();
 
-  $('#main').on('click', function(){
 
-    if ($('.appetizers').is(":visible")) {
-      $('.appetizers').fadeOut(0);
-      $('.main').fadeIn(1500);
-    }
-    if ($('.dessert').is(":visible")) {
-      $('.dessert').fadeOut(0);
-      $('.main').fadeIn(1500);
-    }
-  })
+  $('.chief_picture2').hide();
+  $('.chief_details_2').hide();
 
-  $('#appetizers').on('click', function(){
-    if ($('.main').is(":visible")) {
-      $('.main').fadeOut(0);
-      $('.appetizers').fadeIn(1500);
-    }
-    if ($('.dessert').is(":visible")) {
-      $('.dessert').fadeOut(0);
-      $('.appetizers').fadeIn(1500);
-    }
-  })
 
-  $('#dessert').on('click', function(){
-    if ($('.main').is(":visible")) {
-      $('.main').fadeOut(0);
-      $('.dessert').fadeIn(1500);
-    }
-    if ($('.appetizers').is(":visible")) {
-      $('.appetizers').fadeOut(0);
-      $('.dessert').fadeIn(1500);
-    }
+  $('.our_sous_chef').on('click', function(){
+    $('.chief_picture').fadeOut(0);
+    $('.chief_details_1').fadeOut(0);
+
+    $('.chief_picture2').fadeIn(1500);
+    $('.chief_details_2').fadeIn(1500);
   })
-  $('.onglet').on('hover', function(){
-    $(this).animate({
-      'text-decoration': 'underline !important'
-    }, 1500);
+  $('.our_chef').on('click', function(){
+    $('.chief_picture2').fadeOut(0);
+    $('.chief_details_2').fadeOut(0);
+
+    $('.chief_picture').fadeIn(1500);
+    $('.chief_details_1').fadeIn(1500);
   })
 });
