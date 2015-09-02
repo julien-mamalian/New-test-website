b1e61ed4ffb73df1d47129bde3c436ff6b8d88a9
julien mamalian <julien.mamalian@gmail.com>
2015-09-02 21:55:33 +0200

    page 4

--- source/javascripts/site_2_margin_top_page_3.js
+++ source/javascripts/site_2_margin_top_page_3.js
@@ -9,16 +9,17 @@ $(document).ready(function(){
   var calculus_1 = bar + picture + description - windows_height;
   $('.page_3').css({'margin-top': Math.max(0, calculus_1 ) });
 
-  // Taille des parties du menu 2
-  var picture_meal = $('.page_3 > .flexslider').height();
-  console.log(picture_meal);
-  var details_restaurant = $('.restaurant_details > div:nth-child(1)').height();
-  console.log(details_restaurant);
-  var detail = $('.restaurant_details').height();
-  console.log(detail);
+  // // Taille des parties du menu 2
+  // var windows_height_2 = $('.page_3').height();
+  // var picture_meal = $('.page_3 > .flexslider').height();
+  // var details_restaurant = $('#the_restaurant').height();
 
-  // Calcul du margin top
-  var calculus_2 = picture_meal + details_restaurant - windows_height;
-  console.log(calculus_2)
-  $('.page_4').css({'margin-top': Math.max(0, calculus_2 ) });
+  // console.log(details_restaurant + "the restau")
+  // console.log(picture_meal)
+
+
+  // // Calcul du margin top
+  // var calculus_2 = picture_meal + details_restaurant - windows_height_2;
+  // console.log(calculus_2 + 'calcul')
+  // $('.page_4').css({'margin-top': Math.max(0, calculus_2 ) });
 });
