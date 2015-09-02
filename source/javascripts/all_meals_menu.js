ca69acef7da0063d21b7aa6fc7e71753d4b0b14a
julien mamalian <julien.mamalian@gmail.com>
2015-09-02 21:17:54 +0200

    menu responsive

---
+++ source/javascripts/all_meals_menu.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+  $('.main_meals').hide();
+  $('.dessert_meals').hide();
+
+  $('.one_4').on('click', function(){
+    $('ul').find('active_meal').removeClass('active_meal');
+    $('.all_meals').find('.active_section').fadeOut(0).removeClass('active_section');
+
+    $('.one_4').addClass('active_meal');
+    $('.appetizer_meals').fadeIn(1200).addClass('active_section');
+  });
+
+  $('.two_4').on('click', function(){
+    $('ul').find('.active_meal').removeClass('active_meal');
+    $('.all_meals').find('.active_section').fadeOut(0).removeClass('active_section');
+
+    $('.two_4').addClass('active_meal');
+    $('.main_meals').fadeIn(1200).addClass('active_section');
+  });
+
+  $('.three_4').on('click', function(){
+    $('ul').find('.active_meal').removeClass('active_meal');
+    $('.all_meals').find('.active_section').fadeOut(0).removeClass('active_section');
+
+    $('.three_4').addClass('active_meal');
+    $('.dessert_meals').fadeIn(1200).addClass('active_section');
+  });
+});
--- source/javascripts/site_2_margin_top_page_3.js
+++ source/javascripts/site_2_margin_top_page_3.js
@@ -6,7 +6,19 @@ $(document).ready(function(){
   var description = $('.chief_mobile_1 > .chief_details_1').height();
   var bar = $('.page_2 > .chieves_nav').height();
   // Calcul du margin top
-  var calculus = bar + picture + description - windows_height;
-  console.log(calculus)
-  $('.page_3').css({'margin-top': Math.max(0, calculus ) });
+  var calculus_1 = bar + picture + description - windows_height;
+  $('.page_3').css({'margin-top': Math.max(0, calculus_1 ) });
+
+  // Taille des parties du menu 2
+  var picture_meal = $('.page_3 > .flexslider').height();
+  console.log(picture_meal);
+  var details_restaurant = $('.restaurant_details > div:nth-child(1)').height();
+  console.log(details_restaurant);
+  var detail = $('.restaurant_details').height();
+  console.log(detail);
+
+  // Calcul du margin top
+  var calculus_2 = picture_meal + details_restaurant - windows_height;
+  console.log(calculus_2)
+  $('.page_4').css({'margin-top': Math.max(0, calculus_2 ) });
 });
