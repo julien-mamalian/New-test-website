8697aee203b5b7f458cd50cff8098ee05108c755
julien mamalian <julien.mamalian@gmail.com>
2015-09-06 13:27:13 +0200

    margin top last menu

--- source/javascripts/menu_5.js
+++ source/javascripts/menu_5.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
   //Taille de l'écran
-  var windows_height = $('.page_1').height();
-  // Taille des parties du menu 2
-  var menu_height = $('.page_4 > .menu_zone').height();
-  console.log(menu_height)
+  $(document).on('scroll', function(){
+    var windows_height = $('.page_1').height();
+    // Taille des parties du menu 2
+    var menu_height = $('.page_4 > .menu_zone').height();
+    console.log(menu_height)
 
-  // Calcul du margin top
-  var calculus = menu_height - windows_height;
+    // Calcul du margin top
+    var calculus = menu_height - windows_height;
 
-  $('.page_5').css({
-    'margin-top': Math.max(0, calculus )
-  });
+    $('.page_5').css({
+      'margin-top': Math.max(0, calculus )
+    });
+  })
 });
--- source/javascripts/site_2_margin_top_page_3.js
+++ source/javascripts/site_2_margin_top_page_3.js
@@ -1,26 +1,13 @@
 $(document).ready(function(){
-  //Taille de l'écran
-  var windows_height = $('.page_1').height();
-  // Taille des parties du menu 2
-  var picture = $('.chief_mobile_1 > .chief_picture').height();
-  var description = $('.chief_mobile_1 > .chief_details_1').height();
-  var bar = $('.page_2 > .chieves_nav').height();
-  // Calcul du margin top
-  var calculus_1 = bar + picture + description - windows_height;
-  $('.page_3').css({'margin-top': Math.max(0, calculus_1 ) });
-
-
-  // // Taille des parties du menu 2
-  // var windows_height_2 = $('.page_3').height();
-  // var picture_meal = $('.page_3 > .flexslider').height();
-  // var details_restaurant = $('#the_restaurant').height();
-
-  // console.log(details_restaurant + "the restau")
-  // console.log(picture_meal)
-
-
-  // // Calcul du margin top
-  // var calculus_2 = picture_meal + details_restaurant - windows_height_2;
-  // console.log(calculus_2 + 'calcul')
-  // $('.page_4').css({'margin-top': Math.max(0, calculus_2 ) });
+  $(document).on('scroll', function(){
+    //Taille de l'écran
+    var windows_height = $('.page_1').height();
+    // Taille des parties du menu 2
+    var picture = $('.chief_mobile_1 > .chief_picture').height();
+    var description = $('.chief_mobile_1 > .chief_details_1').height();
+    var bar = $('.page_2 > .chieves_nav').height();
+    // Calcul du margin top
+    var calculus_1 = bar + picture + description - windows_height;
+    $('.page_3').css({'margin-top': Math.max(0, calculus_1 ) });
+  })
 });
