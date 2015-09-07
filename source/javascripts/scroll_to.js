b62c3b01cd2ee57ec7251c31e6c412279814ba5f
julien mamalian <julien.mamalian@gmail.com>
2015-09-07 22:06:37 +0200

    anchors

---
+++ source/javascripts/scroll_to.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  $("#anchor1").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#page2").offset().top
+      }, 1000);
+      return false;
+  });
+
+  $("#anchor2").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#page3").offset().top
+      }, 1000);
+      return false;
+  });
+  $("#anchor3").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#page4").offset().top
+      }, 1000);
+      return false;
+  });
+  $("#anchor4").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#page5").offset().top
+      }, 1000);
+      return false;
+  });
+  $("#anchor5").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#page6").offset().top
+      }, 1000);
+      return false;
+  });
+})
