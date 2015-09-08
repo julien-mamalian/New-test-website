f55cb5fbdc0b0317f6683579f155fa89595c2a34
julien mamalian <julien.mamalian@gmail.com>
2015-09-08 10:48:42 +0200

    menu test

--- source/javascripts/interactive_menu.js
+++ source/javascripts/interactive_menu.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
   $('.menu').hide();
   $('.closed').on('click', function(){
-    $('.menu').hide();
+    $('.menu').fadeToggle();
     $('.menu_link').show();
+    $('.page_1').animate({
+      'margin-top': '0px'
+    }, 800)
   });
+
   $('.menu_link').on('click', function(){
-    $('.menu').show();
+    $('.menu').slideToggle(800);
+    $('.page_1').animate({
+      'margin-top': '150px'
+    }, 800)
     $(this).hide();
   });
 });
