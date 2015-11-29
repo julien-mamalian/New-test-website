db6b561cbd7fdce963576e890446023798ab918b
julien mamalian <julien.mamalian@gmail.com>
2015-11-29 19:30:11 +0100

    menu links

--- source/javascripts/interactive_menu.js
+++ source/javascripts/interactive_menu.js
@@ -3,21 +3,38 @@ $(document).ready(function(){
 
   $('.chieves_nav_ul').on('click', function(){
     MenuWidth = $('.menu').width() * -1;
+    $('#fullpage').addClass('MenuActive');
     $('.menu').animate({
       'right': 0
-    }, 1500);
+    }, 1000);
     $('[id^="page"]').animate({
       'left': MenuWidth
-    }, 1500);
+    }, 1000);
   });
   $('.closed').on('click', function(){
     MenuWidth = $('.menu').width() * -1;
     $('.menu').animate({
       'right': MenuWidth
-    }, 1500);
+    }, 1000);
     $('[id^="page"]').animate({
       'left': '0'
-    }, 1500);
+    }, 1000);
+    $('#fullpage').removeClass('MenuActive');
   });
 
+  $('.rubriques li').on('click', function(event){
+    event.preventDefault();
+    var LinkTo = $(this).find('.link_menu').attr('href');
+    MenuWidth = $('.menu').width() * -1;
+    $('.menu').animate({
+      'right': MenuWidth
+    }, 1000);
+    $('[id^="page"]').animate({
+      'left': '0'
+    }, 1000);
+    $('html, body').delay(1000).animate({
+      scrollTop: $(LinkTo).offset().top
+    }, 1000);
+    $('#fullpage').removeClass('MenuActive');
+  });
 });
