b8c5dc0d59f2fd5caafea8c3497e7ac2959eeee5
julien mamalian <julien.mamalian@gmail.com>
2015-11-29 20:01:12 +0100

    menu nav

--- source/javascripts/interactive_menu.js
+++ source/javascripts/interactive_menu.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
+
+  // Hidden overflow
+  // Add menu bouton
+  // Finish menu design
+
+
+
   var MenuWidth = $('.menu').width() * -1;
 
   $('.chieves_nav_ul').on('click', function(){
     MenuWidth = $('.menu').width() * -1;
-    $('#fullpage').addClass('MenuActive');
+
+    $('#activated_zone').css({
+      'display': 'block'
+    })
+
     $('.menu').animate({
       'right': 0
     }, 1000);
@@ -11,21 +22,27 @@ $(document).ready(function(){
       'left': MenuWidth
     }, 1000);
   });
+
+
   $('.closed').on('click', function(){
     MenuWidth = $('.menu').width() * -1;
+    $('#activated_zone').css({
+      'display': 'none'
+    });
     $('.menu').animate({
       'right': MenuWidth
     }, 1000);
     $('[id^="page"]').animate({
       'left': '0'
     }, 1000);
-    $('#fullpage').removeClass('MenuActive');
+
+
   });
 
-  $('.rubriques li').on('click', function(event){
-    event.preventDefault();
-    var LinkTo = $(this).find('.link_menu').attr('href');
-    MenuWidth = $('.menu').width() * -1;
+  $('#activated_zone').on('click',function(){
+    $(this).css({
+      'display': 'none'
+    })
     $('.menu').animate({
       'right': MenuWidth
     }, 1000);
@@ -35,6 +52,25 @@ $(document).ready(function(){
     $('html, body').delay(1000).animate({
       scrollTop: $(LinkTo).offset().top
     }, 1000);
-    $('#fullpage').removeClass('MenuActive');
+
+  });
+
+  $('.rubriques li').on('click', function(event){
+    event.preventDefault();
+    var LinkTo = $(this).find('.link_menu').attr('href');
+    MenuWidth = $('.menu').width() * -1;
+    $('#activated_zone').css({
+      'display': 'none'
+    })
+    $('.menu').animate({
+      'right': MenuWidth
+    }, 1000);
+    $('[id^="page"]').animate({
+      'left': '0'
+    }, 1000);
+    $('html, body').delay(1000).animate({
+      scrollTop: $(LinkTo).offset().top
+    }, 1250);
+
   });
 });
