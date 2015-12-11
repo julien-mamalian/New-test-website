ccf87fe3de2f419edf0f124bc32523a43e6aad36
julien mamalian <julien.mamalian@gmail.com>
2015-12-11 13:30:25 +0100

    modal menu scroll solving

--- source/javascripts/modal_menu.js
+++ source/javascripts/modal_menu.js
@@ -4,9 +4,11 @@ $(document).ready(function(){
 
   $('.menu_text').on('click',function() {
     $('.modal_menu').show();
-    $('body').css({ 'overflow' : 'hidden'})
+    $.fn.fullpage.destroy();
+    $('body').css({ 'overflow-y' : 'hidden'});
   });
   $('.cross_zone').on('click',function() {
     $('.modal_menu').hide();
+    $('#fullpage').fullpage();
   });
 });
